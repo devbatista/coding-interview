01eece59d5abb18bc069904a3f58554e92ad8fd2
Rafael Batista <rafael@devbatista.com>
2025-06-12 17:47:53 -0300

    Corrigindo os filtros na model de usuários

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   belongs_to :company
 
-  scope :by_company, -> (identifier) { where(company: identifier) if identifier.present? }
-  scope :by_username, -> (username) { where('username LIKE ?', username) if username.present? }
+  scope :by_company, -> (company_id) { where(company: company_id) if company_id.present? }
+  scope :by_username, -> (username) { 
+    where('username ILIKE ?', "%#{username}%") if username.present? 
+  }
 end
