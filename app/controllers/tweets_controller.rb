123699a33fcec8023f6b45612c4fe1b08a1552b4
Rafael Batista <rafael@devbatista.com>
2025-06-12 22:46:11 -0300

    listando os tweets ordenando pelo mais recente paginando com o cursor

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,17 @@
 class TweetsController < ApplicationController
   def index
-    render json: Tweet.all
+    per_page = (params[:per_page] || 10).to_i
+    cursor = params[:cursor]
+
+    tweets = Tweet.order(created_at: :desc)
+    tweets = tweets.where('created_at < ?', Time.at(cursor.to_i)) if cursor.present?
+
+    tweets.limit(per_page)
+    next_cursor = tweets.last&.created_at&.to_i
+
+    render json: {
+      tweets: tweets,
+      next_cursor: (tweets.size == per_page ? next_cursor : nil)
+    }
   end
 end
