5c1571ea4e87dba5a34893a2e429ce13f275d5a9
Rafael Batista <rafael@devbatista.com>
2025-06-13 01:04:41 -0300

    correção das rotas para que a 'new' seja criada

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :companies do
+  resources :companies, shallow: true do
+    collection do
+      get 'new', to: 'companies#new', as: :new
+    end
+    member do
+      get 'edit', to: 'companies#edit', as: :edit
+    end
     resources :users, only: [:index]
   end
 
