c921baa05915dbf767443f723350858b6e203bee
Rafael Batista <rafael@devbatista.com>
2025-06-14 08:26:46 -0300

    movendo as regras para a model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,12 @@
 class Tweet < ApplicationRecord
+  belongs_to :user
+
+  scope :recent, -> { order(created_at: :desc) }
+  scope :before_cursor, ->(cursor) { 
+    where('created_at < ?', Time.at(cursor.to_i)) if cursor.present?
+  }
+  scope :by_username, ->(username) {
+    joins(:user).where(users: { username: username }) if username.present?
+  }
+  scope :paginate, ->(per_page) { limit(per_page) if per_page.present? }
 end
