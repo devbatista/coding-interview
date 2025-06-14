64587d5b63bb449133506d87423386eb6afbb802
Rafael Batista <rafael@devbatista.com>
2025-06-14 08:28:13 -0300

    ajustando controller de tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,29 +1,20 @@
 class TweetsController < ApplicationController
-  before_action :set_tweets
-
+  
   def index
     per_page = (params[:per_page] || 10).to_i
     cursor = params[:cursor]
+    username = params[:user_username]
 
-    @tweets = @tweets.where('created_at < ?', Time.at(cursor.to_i)) if cursor.present?
+    @tweets = @tweets.recent
+                     .by_username(username)
+                     .before_cursor(cursor)
+                     .paginate(per_page)
 
-    @tweets = @tweets.limit(per_page)
     next_cursor = @tweets.last&.created_at&.to_i
 
     render json: {
       tweets: @tweets,
       next_cursor: (@tweets.size == per_page ? next_cursor : nil)
     }
-  end
-
-  private
-
-  def set_tweets
-    if params[:user_username]
-      user = User.find_by(username: params[:user_username])
-      @tweets = user.tweets.order(created_at: :desc)
-    else
-      @tweets = Tweet.order(created_at: :desc)
-    end
   end
 end
