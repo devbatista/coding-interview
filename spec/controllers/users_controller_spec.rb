bb6620be65beeb6d4664d1dac1c00f404a36ea6f
Rafael Batista <rafael@devbatista.com>
2025-06-14 09:50:32 -0300

    Ajustes no teste de controller de usuarios

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,22 +7,22 @@ RSpec.describe UsersController, type: :controller do
   let!(:users_company_2) { create_list(:user, 5, company: company_2) }
 
   describe "GET #index" do
-    context "quando busca usuários por empresa" do
-      it "atribui apenas os usuários da empresa especificada a @users" do
+    context "when searching for users by company" do
+      it "assigns only users from the specified company to @users" do
         get :index, params: { company_identifier: company_1.id }
         expect(assigns(:users)).to match_array(users_company_1)
       end
     end
 
-    context "quando busca todos os usuários" do
-      it "atribui todos os usuários a @users" do
+    context "when searching for all users" do
+      it "assigns all users to @users" do
         get :index
         expect(assigns(:users)).to match_array(User.all)
       end
     end
 
-    context "quando busca por username" do
-      it "atribui apenas os usuários com o username buscado a @users" do
+    context "when searching by username" do
+      it "assigns only users with the searched username to @users" do
         user = users_company_1.first
         get :index, params: { username: user.username }
         expect(assigns(:users)).to match_array([user])
