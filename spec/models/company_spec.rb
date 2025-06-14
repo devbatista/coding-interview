b519dea5da1b043d627a930a447282cef18fd4cb
Rafael Batista <rafael@devbatista.com>
2025-06-14 08:51:54 -0300

    acrescentando testes da model Company

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is invalid without name" do
+      company = Company.new(name: nil)
+      expect(company).not_to be_valid
+      expect(company.errors[:name]).to(include("can't be blank"))
+    end
+
+    it "is invalid with duplicate name" do
+      Company.create!(name: "Company X")
+      company2 = Company.new(name: "Company X")
+      expect(company2).not_to be_valid
+      expect(company2.errors[:name]).to(include("has already been taken"))
+    end
+
+    it "is valid with unique name" do
+      company = Company.new(name: "Company Y")
+      expect(company).to(be_valid)
+    end
+  end
+
+  describe "associations" do
+    it "can have many users" do
+      company = Company.create!(name: "Company Z")
+      user1 = User.create!(username: "user1", company: company)
+      user2 = User.create!(username: "user2", company: company)
+      expect(company.users).to(include(user1, user2))
+    end
+  end
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -12,35 +12,35 @@ RSpec.describe Tweet, type: :model do
 
   describe ".recent" do
     it "returns tweets from newest to oldest" do
-      expect(Tweet.recent).to eq([tweet2, tweet1, tweet3])
+      expect(Tweet.recent).to(eq([tweet2, tweet1, tweet3]))
     end
   end
 
   describe ".by_username" do
     it "filtra tweets pelo username do usuário" do
-      expect(Tweet.by_username("user1")).to match_array([tweet1, tweet2])
-      expect(Tweet.by_username("user2")).to match_array([tweet3])
+      expect(Tweet.by_username("user1")).to(match_array([tweet1, tweet2]))
+      expect(Tweet.by_username("user2")).to(match_array([tweet3]))
     end
 
     it "returns all if username is not provided" do
-      expect(Tweet.by_username(nil)).to match_array([tweet1, tweet2, tweet3])
+      expect(Tweet.by_username(nil)).to(match_array([tweet1, tweet2, tweet3]))
     end
   end
 
   describe ".before_cursor" do
     it "returns tweets created before the cursor" do
       cursor = tweet2.created_at.to_i
-      expect(Tweet.before_cursor(cursor)).to match_array([tweet1, tweet3])
+      expect(Tweet.before_cursor(cursor)).to(match_array([tweet1, tweet3]))
     end
 
     it "returns all if cursor is not given" do
-      expect(Tweet.before_cursor(nil)).to match_array([tweet1, tweet2, tweet3])
+      expect(Tweet.before_cursor(nil)).to(match_array([tweet1, tweet2, tweet3]))
     end
   end
 
   describe ".paginate" do
     it "limits the amount of tweets" do
-      expect(Tweet.paginate(2).size).to eq(2)
+      expect(Tweet.paginate(2).size).to(eq(2))
     end
   end
 end
