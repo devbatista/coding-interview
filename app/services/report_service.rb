60086266332c9b04a77d70ee000260803aec9ee1
Rafael Batista <rafael@devbatista.com>
2025-06-14 08:58:26 -0300

    adicionando um relatório pra ser usado no ReportService

--- app/services/report_service.rb
+++ app/services/report_service.rb
@@ -1,9 +1,17 @@
+require 'csv'
+
 class ReportService
   def initialize(user_id)
     @user = User.find(user_id)
   end
 
   def generate
-    
+    tweets = @user.tweets.recent
+    CSV.generate(headers: true) do |csv|
+      csv << ['Content', "Date"]
+      tweets.each do |tweet|
+        csv << [tweet.body, tweet.created_at.strftime("%d/%m/%Y %H:%M")]
+      end
+    end
   end
 end
